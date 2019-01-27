3fa26ab6b4b61f075bc55baa3b6bddc5a1550c00
jingjingxyk <zonghengbaihe521@qq.com>
2019-01-27 22:11:22 +0800

    dockerfile file change

--- git-push.sh
+++ git-push.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 git add .
-git commit -a -m "add file"
-
+git commit -a -m "dockerfile file change "
+#git push -u origin master
 
 
 TIME=`date "+%Y%m%d%H%M"`
 GIT_REVISION=`git log -1 --pretty=format:"%h"`
-IMAGE_NAME=release-vdownhelp:${TIME}_${GIT_REVISION}
-echo ${IMAGE_NAME}
-#git push -u origin master
+tag_name=release-vdownhelp_${TIME}_${GIT_REVISION}
+echo ${tag_name}
+
+
+#git tag
+
+#git push origin
 #docker build -t registry.cn-beijing.aliyuncs.com/jingjingxyk/ubuntu:latest -f ./Dockerfile .
 #docker push registry.cn-beijing.aliyuncs.com/jingjingxyk/ubuntu:latest
