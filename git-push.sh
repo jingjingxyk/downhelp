d15990550dd559ccd3c1b2d95e038dedc78e4768
jingjingxyk <zonghengbaihe521@qq.com>
2019-01-27 22:09:02 +0800

    add file

--- git-push.sh
+++ git-push.sh
@@ -6,7 +6,7 @@ git commit -a -m "add file"
 
 TIME=`date "+%Y%m%d%H%M"`
 GIT_REVISION=`git log -1 --pretty=format:"%h"`
-IMAGE_NAME=downhelp:${TIME}_${GIT_REVISION}
+IMAGE_NAME=release-vdownhelp:${TIME}_${GIT_REVISION}
 echo ${IMAGE_NAME}
 #git push -u origin master
 #docker build -t registry.cn-beijing.aliyuncs.com/jingjingxyk/ubuntu:latest -f ./Dockerfile .
